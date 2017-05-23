db09f71216ce63a7eb9c5263b2b8ce64bf035e1d
Michael Axelson <axelmichael11@gmail.com>
2017-05-23 14:11:43 -0700

    added the github token

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -9,7 +9,16 @@ var app = app || {};
 
     // TODO: Call a function to load all the data.
     // Pass a view function as a callback, so the view will render after the data is loaded.
-
+    $.ajax({
+      url: 'https://api.github.com/user/repos',
+      method: 'GET',
+      headers: {
+        Authorization: `token ${myGitHubToken}`,
+      }
+      .then(
+        console.log('test');
+      )
+    })
   };
 
   module.aboutController = aboutController;
